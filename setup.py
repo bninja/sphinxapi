548276889edf7894ab88032fbc9ea8d7ca6825fd
Andrew <andrew@poundpay.com>
2011-12-12 22:24:38 -0800

    .

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='sphinxapi',
+    version='2.0.2',
+    description='Python version of Sphinx searchd client (Python API)',
+    author='Sphinx Technologies Inc',
+    url='http://sphinxsearch.com',
+    py_modules=['sphinxapi'],
+)
